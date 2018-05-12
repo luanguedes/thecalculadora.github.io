7909d67db0a76396feb27ab76904bc7e570646b5
Luan <luan.guedes@edu.unipar.br>
2018-05-12 18:14:07 -0300

    commit multiplicação / inicio CSS

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,13 +1,40 @@
-function botao (num) 
-{
-	var salvo = document.calc.visor.value ;
-	document.calc.visor.value = salvo + num;
-}
+var Acumm = 0;
+var FlagNewNum = false;
+var PendingOp = "";
 
-function reset ()
+function botao(num) 
 {
+	if (FlagNewNum){
+		document.calc.visor.value = num ;
+		FlagNewNum = false;
+	}
+	else{
+		if (document.calc.visor.value == "0")
+			document.calc.visor.value = num;
+		else 
+			document.calc.visor.value += num;
+	}
 	
 }
-function calcule(){
-
+function reset()
+{	
+}
+function op(operador){
+	if (document.calc.visor.value == "") {
+		alert("O campo esta vazio, digite um calculo!");
+		document.calc.visor.value = "0";
+	}
+	else{
+		if (FlagNewNum && PendingOp != "=");
+		else{
+			FlagNewNum = true;
+			if('*' == PendingOp){
+				Accum *= parseFloat(document.calc.visor.value);
+			}
+			else
+				Accum = parseFloat(document.calc.visor.value);
+				document.calc.visor.value = Accum;
+				PendingOp = operador;
+		}
+	}
 }
